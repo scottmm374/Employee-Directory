1ca81c5f3ecd64f241567b23977746fd53dd7159
Michelle Scott <scottmm374@gmail.com>
2021-03-14 17:24:27 -0400

    post new employee works

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import EmployeeList from './components/views/EmployeeList'
 import Employee from './components/views/Employee'
+import AddEmployee from './components/forms/AddEmployee'
 
 function App() {
   return (
     <div className="App">
       <EmployeeList />
       <Employee />
+      <AddEmployee />
     </div>
   );
 }
