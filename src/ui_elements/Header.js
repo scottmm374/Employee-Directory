2b0fa81980636b700fc2cb2a7f198234d11a1dee
Michelle Scott <scottmm374@gmail.com>
2021-03-25 11:18:26 -0400

    added styling to add emp button in header

--- src/ui_elements/Header.js
+++ src/ui_elements/Header.js
@@ -5,9 +5,11 @@ import rosie from '../assets/rosie.svg'
  const Header = () => {
      return(
          <div className='header'>
-             <img src={rosie} alt="rosie"/>
              <h1>Team Directory</h1>
-             <div><Link to='/add-employee'><button>Add New Employee</button></Link></div>
+             {/* <img src={rosie} alt="rosie"/> */}
+             
+             <div><Link to='/add-employee'><button className="add-employee-button"><span></span><span></span><span></span><span></span>Add New Employee</button></Link></div>
+             <img src={rosie} alt="rosie"/>
              
          </div>
      )
