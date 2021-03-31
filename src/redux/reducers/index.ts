a9a477d944cc1d09e6caec74e67f290e32226ee9
Michelle Scott <scottmm374@gmail.com>
2021-03-31 01:03:54 -0400

    Revert "Ts integrate"

--- src/redux/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { getEmployeesReducer } from './EmployeesReducer';
-
-const rootReducer = combineReducers({
-  employees: getEmployeesReducer
-});
-
-export default rootReducer;
