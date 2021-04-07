8901c2a63313fb89b98da6e8c8f4a347c53a396c
Michelle <scottmm374@gmail.com>
2021-04-07 11:42:44 -0400

    added style to card icons, black backgrounds, off edge icons

--- src/components/views/EmployeeCard.tsx
+++ src/components/views/EmployeeCard.tsx
@@ -24,24 +24,24 @@ export default function EmployeeCard(): JSX.Element {
        
          {employees.map(employee => (
               <Link to={`/employee/${employee.id}`}>
-                    <Card  style={{width: "15rem"}} className='card' key={employee.id}>
+                    <Card  style={{width: "15rem"}} className='card-main' key={employee.id}>
                         <Card.Body>
 
                         <div className='top-card-section'>
                        <Card.Img style={{width: "160px"}} className="card-img" src={`${employee.photo}`} alt={employee.first_name}/>
                         {/* <div className="name-card"> */}
                             <Card.Subtitle className="first-name-card">{employee.first_name} {employee.last_name}</Card.Subtitle> 
-                            
-                       <Card.Text className="icons-card">
+                            </div>  
+                       <Card.Text  className="card-icons-container">
 
                       
                         {/* <div className='icons-card'> */}
-                         <p className="card-icons" ><FontAwesomeIcon icon={faPhoneAlt} /></p>
-                        <p className="card-icons"><FontAwesomeIcon icon={faEnvelope} /></p>
+                         <p className="card-icons-circle" ><FontAwesomeIcon icon={faPhoneAlt} /></p>
+                        <p className="card-icons-circle"><FontAwesomeIcon icon={faEnvelope} /></p>
                        
                         </Card.Text>
                         {/* <Link to={`/employee/${employee.id}`}><button className="view-profile-button">View</button></Link> */}
-            </div>
+            
                         </Card.Body>
                         
                         
