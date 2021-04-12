8618ed60f643246700de3383a3e5c2d3e8fcf14b
Michelle <scottmm374@gmail.com>
2021-04-12 17:57:15 -0400

    added rows and cols to profile page, and alignments

--- src/components/views/ProfilePage.tsx
+++ src/components/views/ProfilePage.tsx
@@ -5,6 +5,7 @@ import {Link} from 'react-router-dom'
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {Container, Col, Row} from 'react-bootstrap'
+import { reduceEachLeadingCommentRange } from 'typescript';
 
 // TODO: Media breakpoints for fonts, responsive header, last name resizing 
 // FIXME: Edit classname acting strange, added addButton, Need to fix
@@ -33,37 +34,37 @@ export default function ProfilePage(props:any): JSX.Element {
                 <Col xs={8}>
                 <h1>{currentEmployee.first_name}</h1>
                 </Col>
-                <Col xs={4}>
+                <Col xs={true}>
                 <Link to="/"><FontAwesomeIcon icon={faHome} className="home-button" size="3x"/></Link>
                 </Col> 
                
             </Row>
             
         
-            <Row className="main-content">
-                <Col xs={4} className="left-side">
-                    <p><img src={`${currentEmployee.photo}`} alt={currentEmployee.first_name}/></p>
-                    <h4>{currentEmployee.first_name} {currentEmployee.last_name}</h4>
-    {/* FIXME: Buttons not right(naming) */}
-                    <div><Link to="/update-employee"><button className="edit-button"><span></span><span></span><span></span><span></span>Edit Profile</button></Link></div>
-                    
+            <Row className="main-content" >
+                <Col xs={5} className="left-side">
+                    <Container fluid >
+                    <Row><p><img src={`${currentEmployee.photo}`} alt={currentEmployee.first_name}/></p></Row>
+                    <Row><Col xs={6}><h4 className="first-name">{currentEmployee.first_name}</h4></Col><Col xs={6}><h4 className="last-name">{currentEmployee.last_name}</h4></Col></Row>
+                    <Row><Col><Link to="/update-employee"><button className="edit-button"><span></span><span></span><span></span><span></span>Edit Profile</button></Link></Col></Row>
+                    </Container>
                 </Col>
 
-                <Col  xs={1} className="sideways-profile-name">
+                <Col  xs={true} className="sideways-profile-name">
                     <h2>{currentEmployee.last_name}</h2>
                 </Col>
 
-                <Col xs={6} className="right-side">
+                <Col xs={5} className="right-side">
                     <Container fluid className="information">
-                        <Row><Col lg={true}><h4>Phone:</h4></Col><Col lg={true}><p>{currentEmployee.phone}</p></Col></Row>
-                        <Row><Col lg={true}><h4>Email: </h4></Col><Col lg={true}><p>{currentEmployee.email}</p></Col></Row>
-                        <Row><Col lg={true}><h4>Street Address: </h4></Col><Col lg={true}><p>{currentEmployee.address}</p></Col></Row>
-                        <Row><Col lg={true}><h4>City :</h4></Col><Col lg={true}><p>{currentEmployee.city}</p></Col></Row> 
-                        <Row><Col lg={true}><h4>State:</h4></Col><Col lg={true}><p>{currentEmployee.state}</p></Col></Row>
-                        <Row><Col lg={true}><h4>Zip Code:</h4></Col><Col lg={true}><p>{currentEmployee.zip}</p></Col></Row>
+                        <Row><Col xs={6}><h4>Phone:</h4></Col><Col xs={true}><p>{currentEmployee.phone}</p></Col></Row>
+                        <Row><Col xs={6}><h4>Email: </h4></Col><Col xs={true}><p>{currentEmployee.email}</p></Col></Row>
+                        <Row><Col xs={6}><h4>Street Address: </h4></Col><Col xs={true}><p>{currentEmployee.address}</p></Col></Row>
+                        <Row><Col xs={6}><h4>City :</h4></Col><Col xs={true}><p>{currentEmployee.city}</p></Col></Row> 
+                        <Row><Col xs={6}><h4>State:</h4></Col><Col xs={true}><p>{currentEmployee.state}</p></Col></Row>
+                        <Row><Col xs={6}><h4>Zip Code:</h4></Col><Col xs={true}><p>{currentEmployee.zip}</p></Col></Row>
                     </Container>
                         
-                    <div className="notes"><h4>Notes</h4><br/><br/>{currentEmployee.notes}</div>
+                    <Row><div className="notes"><h4>Notes</h4><br/><br/>{currentEmployee.notes}</div></Row>
                 </Col>
 
             </Row>
